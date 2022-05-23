72d0ed8fd94996e7175ba401046b8ad1a02558da
devFalah <devfalahhassan@gmail.com>
2022-05-23 10:16:13 +0300

    format code

--- src/main/kotlin/models/App.kt
+++ src/main/kotlin/models/App.kt
@@ -1,6 +1,12 @@
 package models
 
-class App(val name:String,val company:String,val category: String,val updatedAt:String,val size:String,val
-installsCount:Long, val currentVersion:String,val requiresAndroid:String,
-) {
-}
+class App(
+    val name:String,
+    val company:String,
+    val category: String,
+    val updatedAt:String,
+    val size:String,
+    val installsCount:Long,
+    val currentVersion:String,
+    val requiresAndroid:String,
+)
