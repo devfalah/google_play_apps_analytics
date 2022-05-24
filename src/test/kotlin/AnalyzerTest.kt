e357ed7e5816566b2631e3108e2f92d80ed1086f
devFalah <devfalahhassan@gmail.com>
2022-05-24 13:32:23 +0300

    add Analyzer class test

---
+++ src/test/kotlin/AnalyzerTest.kt
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class AnalyzerTest {
+
+    @Test
+    fun calculateCountAppsDevelopedByGoogle() {
+    }
+
+    @Test
+    fun calculatePercentageOfMedicalApps() {
+    }
+
+    @Test
+    fun getOldestApp() {
+    }
+
+    @Test
+    fun calculatePercentageOfAppsRunningOnAndroid9AndUpOnly() {
+    }
+
+    @Test
+    fun getLargest10Apps() {
+    }
+
+    @Test
+    fun getTop10InstalledApps() {
+    }
+}
