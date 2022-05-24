1454e68e2b2e626882a9e885ae06e8abed7f6844
devFalah <devfalahhassan@gmail.com>
2022-05-24 13:30:10 +0300

    add Analyzer class method with no implemente

--- src/main/kotlin/Analyzer.kt
+++ src/main/kotlin/Analyzer.kt
@@ -1,2 +1,23 @@
+import models.App
+
 class Analyzer {
+
+    fun calculateCountAppsDevelopedByGoogle(apps:List<App>):Int{
+        TODO("not implemented")
+    }
+    fun  calculatePercentageOfMedicalApps(apps:List<App>):Double{
+        TODO("not implemented")
+    }
+    fun  getOldestApp(apps:List<App>):App{
+        TODO("not implemented")
+    }
+    fun  calculatePercentageOfAppsRunningOnAndroid9AndUpOnly(apps:List<App>):Double{
+        TODO("not implemented")
+    }
+    fun  getLargest10Apps(apps:List<App>):List<App>{
+        TODO("not implemented")
+    }
+    fun  getTop10InstalledApps(apps:List<App>):List<App>{
+        TODO("not implemented")
+    }
 }
