85301c9a627d339121dbc098b336a8f5de677331
fatima haitham <fatimahaitam2@gmail.com>
2022-05-28 14:30:39 -0700

    add function for getOldestApp methode

--- src/main/kotlin/Analyzer.kt
+++ src/main/kotlin/Analyzer.kt
@@ -1,4 +1,6 @@
 import models.App
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 
 class Analyzer {
 
@@ -9,7 +11,8 @@ class Analyzer {
         TODO("not implemented")
     }
     fun  getOldestApp(apps:List<App>):App?{
-        TODO("not implemented")
+        val formatter: DateFormat = SimpleDateFormat("MMMM dd yy")
+        return apps.minByOrNull { formatter.parse(it.updatedAt) }
     }
     fun  calculatePercentageOfAppsRunningOnAndroid9AndUpOnly(apps:List<App>):Double{
         TODO("not implemented")
