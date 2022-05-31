4123ffcff7c795e19162e1d15adebcc6ad441219
AmeerAmjed <os@localhost.localdomain>
2022-05-31 23:27:36 +0300

    impelement app model

--- src/main/kotlin/models/App.kt
+++ src/main/kotlin/models/App.kt
@@ -4,16 +4,12 @@ package models
 data  class App(
     val name:String,
     val company:String,
-    var category: String,
-    var updatedAt:String,
+    val category: String,
+    val updatedAt:String,
     val size:String,
     val installsCount:Long,
     val currentVersion:String,
-    var requiresAndroid:String,
-) {
-    override fun equals(other: Any?): Boolean {
-        return size.uppercase() == (other as App).size.uppercase()
-    }
-}
+    val requiresAndroid:String,
+) {}
 
 
