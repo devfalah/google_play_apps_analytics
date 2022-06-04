3f6c0959435c30a8db6745788c733065b5c56c89
Musty010 <mstfyjmalalden@gmail.com>
2022-06-04 14:38:46 +0300

    fix extension function that convert string to date

---
+++ src/main/kotlin/utility/extension/extension_convert_string_to_date.kt
@@ -0,0 +1,15 @@
+package utility.extension
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * convert String to date
+ * @return Date Object
+ */
+
+fun String.convertStringToDateObject(): Date {
+    return SimpleDateFormat("MMMM d yyyy").parse(this)
+}
+
+
